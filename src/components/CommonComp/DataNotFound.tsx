a3566115311ef3a0560edb2d4e041be8588e402f
mjaumi <mjaumi2864@gmail.com>
2024-02-11 02:25:45 +0600

    data not found component design done

--- src/components/CommonComp/DataNotFound.tsx
+++ src/components/CommonComp/DataNotFound.tsx
@@ -1,9 +1,7 @@
 const DataNotFound = ({ message }: { message: string }) => {
   // rendering data not found component here
   return (
-    <div>
-      <p>{message}</p>
-    </div>
+    <p className='text-danger text-lg text-center font-medium'>{message}</p>
   );
 };
 
