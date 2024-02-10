4f95a918ecf585c4f6b1254fda40b8e0d05d8ddd
mjaumi <mjaumi2864@gmail.com>
2024-02-11 02:10:56 +0600

    header responsiveness done

--- src/components/CommonComp/Header.tsx
+++ src/components/CommonComp/Header.tsx
@@ -5,14 +5,14 @@ const Header = () => {
   // rendering the header component here
   return (
     <header className='sticky top-0 py-6 shadow-todo-shadow'>
-      <div className='w-4/5 mx-auto flex items-center justify-between'>
+      <div className='w-[95%] lg:w-4/5 mx-auto flex items-center justify-between'>
         <h1 className='text-4xl font-semibold'>
           Todo <span className='text-2xl text-info'>List</span>
         </h1>
 
         <div className='flex items-center gap-2'>
           <FaFilter className='text-info h-6 w-6' />
-          <div className='w-[200px]'>
+          <div className='w-[150px] lg:w-[200px]'>
             <DropdownField options={['', 'Low', 'Medium', 'High']} isFilter />
           </div>
         </div>
